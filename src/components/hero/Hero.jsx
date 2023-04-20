cab0b698f5bd2daa70a554e6e872c67b3a897f9b
hoseinmoradi <moradih7379@gmail.com>
2023-04-20 12:33:44 +0330

    test new components.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -18,6 +18,14 @@ const Hero = () => {
           <div className="content__text">
             <h1>به کافه فودلی خوش آمدید</h1>
             <h1>تلاش ما کسب رضایت شماست</h1>
+
+            <div className="content__social lg">
+              <p>ما را در اینستاگرام دنیال کنید</p>
+              <span>
+                <Svginsta width={30} height={30} />
+              </span>
+              <button>مشاهده منو جهت سفارش</button>
+            </div>
           </div>
 
           <div className="content__img">
@@ -28,7 +36,7 @@ const Hero = () => {
             <span></span>
           </div>
 
-          <div className="content__social">
+          <div className="content__social sm">
             <p>ما را در اینستاگرام دنیال کنید</p>
             <span>
               <Svginsta width={30} height={30} />
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./home.css";
-import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
+import Lists from "../../components/lists/Lists";
 
 const Home = () => {
   return (
     <div className="home__container">
       <section className="home__wrapper">
         <Hero />
+      </section>
+
+      <section className="list__wrapper">
+        <Lists />
       </section>
     </div>
   );
